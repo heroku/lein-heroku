e5bf0687eeaaffec3421d019fa0bd39f159a89ba
Joe Kutner <jpkutner@gmail.com>
2015-07-07 19:30:32 -0500

    Changed license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject lein-heroku "0.4.4"
   :description "Leiningen plugin that deploys to Heroku"
   :url "https://github.com/heroku/lein-heroku"
-  :license {:name "MIT License"
-            :url "http://opensource.org/licenses/MIT"}
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
     [com.heroku.sdk/heroku-deploy "0.4.4"]
     [com.fasterxml.jackson.core/jackson-core "2.2.0"]
