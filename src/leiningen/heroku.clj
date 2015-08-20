23547427bad7ef747aeb67894bdbc1c8c8a7ffdb
Joe Kutner <jpkutner@gmail.com>
2015-08-20 15:10:37 -0500

    Removed support for lein runner

--- src/leiningen/heroku.clj
+++ src/leiningen/heroku.clj
@@ -8,24 +8,14 @@
   (:import [com.heroku.sdk.deploy App]
            [java.io File]))
 
-(def lein-default-profile "{:user  { :offline? true :checksum :ignore
-  :mirrors  {\"local\" {:url \"file:///app/.m2/repository\" :checksum :ignore}}}}")
-
 (defn- root-file [project & [f]] (new File (str (:root project) (File/separator) f)))
 
 (defn- log-warn [msg] (main/warn "WARNING:" msg))
 
-(defn- vendor-dependencies [dependency-key project]
-  (classpath/get-dependencies dependency-key (merge (select-keys project [dependency-key
-    :update :checksum])
-    {:repositories [["central" {:url (str "file:" (.getPath (io/file (System/getProperty "user.home") ".m2" "repository")))}]]
-    :local-repo (java.io.File. (:root project) "target/heroku/app/.m2/repository")})))
-
 (defn- deploy-uberjar
   "Deploy the uberjar to Heroku"
   [project]
-  (let [logLevel (Integer/parseInt (System/getProperty "heroku.logLevel" "3"))
-        app (proxy [App] [
+  (let [app (proxy [App] [
           "lein-heroku"
           (get-in project [:heroku :app-name])
           (root-file project)
@@ -47,33 +37,9 @@
 (defn- deploy-lein
   "Deploy directories and dependencies to Heroku"
   [project]
-  (let [logLevel (Integer/parseInt (System/getProperty "heroku.logLevel" "3"))
-        app (proxy [App] [
-          "lein-heroku"
-          (get-in project [:heroku :app-name])
-          (root-file project)
-          (root-file project "target")
-          ["https://codon-buildpacks.s3.amazonaws.com/buildpacks/heroku/jvm-common.tgz"
-           "https://github.com/jkutner/heroku-buildpack-lein"]]
-        (logWarn [msg] (log-warn msg))
-        (logInfo [msg] (main/info msg))
-        (logDebug [msg] (main/debug msg))
-        (prepare [includedFiles processTypes]
-          (main/info "-----> Vendoring dependencies...")
-          (.mkdirs (java.io.File. (:root project) "target/heroku/app/.lein"))
-          (spit (java.io.File. (:root project) "target/heroku/app/.lein/profiles.clj") lein-default-profile)
-          (vendor-dependencies :dependencies project)
-          (vendor-dependencies :plugins project)
-          (proxy-super prepare includedFiles processTypes) ))]
-    (.deploy app
-      (map (fn [x] (root-file project x)) (or
-        (:include-files (:heroku project))
-        ["target", "src", "resources", "project.clj"]))
-      {}
-      (or (:jdk-version (:heroku project)) "1.8")
-      "cedar-14"
-      (or (:process-types (:heroku project)) {"web" "lein with-profile production trampoline run"})
-      "slug.tgz")))
+  (println "This plugin only supports Uberjar deployment!\n"
+           "See the Leiningen docs for more information:\n"
+           "https://github.com/technomancy/leiningen/blob/master/doc/TUTORIAL.md#uberjar"))
 
 (defn heroku
   "Deploy to Heroku PaaS"
