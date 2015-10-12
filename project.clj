a8b02929f15dc79053c590689c132b0d28e14417
Joe Kutner <jpkutner@gmail.com>
2015-10-12 17:33:30 -0500

    Upgrade to heroku-deploy 0.5.3

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject lein-heroku "0.5.1"
+(defproject lein-heroku "0.5.3"
   :description "Leiningen plugin that deploys to Heroku"
   :url "https://github.com/heroku/lein-heroku"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-    [com.heroku.sdk/heroku-deploy "0.5.1"]
+    [com.heroku.sdk/heroku-deploy "0.5.3"]
     [com.fasterxml.jackson.core/jackson-core "2.2.0"]
     [com.fasterxml.jackson.core/jackson-annotations "2.2.0"]
     [com.fasterxml.jackson.core/jackson-databind "2.2.0"]]
